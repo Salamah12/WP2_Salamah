56fc2a8e85ec78512f59322cb8ae987d16952163
Salamah12 <r.salamahforever@gmail.com>
2021-10-17 14:18:31 +0700

    Latihan Pertemuan 3

---
+++ application/views/View-latihan.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    <h1>Halo Kawan..</h1>
+    <h3>Yuk kita belajar web programming..!!!</h3>
+    Nilai 1 = <?= $nilai1; ?><br>
+    Nilai 2 = <?= $nilai2; ?><br>
+    ini hasil dari permodelan dengan methode penjumlahan yaitu 
+    <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>  
+</body>
+
+</html>
